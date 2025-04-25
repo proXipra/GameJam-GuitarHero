6d125c42c662fe2968e93d9ece13908d4eb9ef39
rendekar8 <abdullah.ozen@ug.bilkent.edu.tr>
2025-04-25 22:38:24 +0300

    Create InputHandler.cs

---
+++ Assets/_Scripts/Managers/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+namespace _Scripts.PlayerControls
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        public static PlayerInputHandler Instance;
+
+        private PlayerInput PlayerInput => _playerInput ??= GetComponent<PlayerInput>();
+        private PlayerInput _playerInput;
+
+        public bool JumpInput { get; set; }
+        
+        public event Action OnJumpInput;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void JumpInput(InputAction.CallbackContext context)
+        {
+            if (context.canceled) JumpInput = false;
+            if (!context.started) return;
+            JumpInput = true;
+            OnJumpInput?.Invoke();
+        }
+    }
+}
