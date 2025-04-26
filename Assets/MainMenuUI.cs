6413c56031ddda636eff0bbe2359129a6539e853
Cihan <cihanbalkr@gmail.com>
2025-04-26 17:44:46 +0300

    mainmenu function

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
