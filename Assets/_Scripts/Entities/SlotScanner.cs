592c5de49b56d926a268bfed8953a72b515eb255
rendekar8 <abdullah.ozen@ug.bilkent.edu.tr>
2025-04-26 16:36:33 +0300

    Update Scene

--- Assets/_Scripts/Entities/SlotScanner.cs
+++ Assets/_Scripts/Entities/SlotScanner.cs
@@ -1,9 +1,26 @@
+using System;
+using _Scripts.PlayerControls;
 using UnityEngine;
 
 namespace _Scripts.Entities
 {
     public class SlotScanner : MonoBehaviour
     {
+        private Vector2 _workSpace = new (4, 1);
         
+        private void Start()
+        {
+            InputHandler.Instance.OnJumpInput += HandleOnJumpInput;
+        }
+
+        private void OnDisable()
+        {
+            InputHandler.Instance.OnJumpInput -= HandleOnJumpInput;
+        }
+
+        private void HandleOnJumpInput()
+        {
+            Physics2D.OverlapBox(transform.position, _workSpace)
+        }
     }
 }
