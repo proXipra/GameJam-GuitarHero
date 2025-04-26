03c97a44d96f9d1a1eab057a040c022198d876da
rendekar8 <abdullah.ozen@ug.bilkent.edu.tr>
2025-04-26 14:56:27 +0300

    Update Slots

---
+++ Assets/_Scripts/Slots/SlotSpawner.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using _Scripts.Manager;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Scripts.Slots
+{
+    public class SlotSpawner : MonoBehaviour
+    {
+        [SerializeField] private Slot slot;
+        [SerializeField] private float distance;
+        
+        private Queue<Slot> _slotInstances = new Queue<Slot>();
+        
+        private void Start()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                var slotInstance = Instantiate(slot, transform);
+                slotInstance.distance = distance;
+                _slotInstances.Enqueue(slotInstance);
+            }
+            InvokeRepeating(nameof(SpawnSlot), 3, GameManager.Instance.Period);
+        }
+
+        private void SpawnSlot()
+        {
+            if (_slotInstances.TryPeek(out _)) _slotInstances.Dequeue().FireSlot(Random.Range(0, 4));
+        }
+    }
+}
